043ad68a563bb9e47d9970e4dd2133d07eb07a44
taka87 <daredevil@dir.bg>
2016-08-14 17:51:37 +0300

    Tem Work ASP MVC Forum

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Blog.Models;
+
+namespace MVC_Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var post = db.Posts.OrderByDescending(p => p.Date).Take(3);
+            return View(post.ToList());
+        }
+    }
+}
