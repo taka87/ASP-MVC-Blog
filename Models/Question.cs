9d149c85de8c4c361fd68205fe65e8acd0d595ad
taka87 <daredevil@dir.bg>
2016-08-23 18:33:43 +0300

    Dobaven Model
    Questions Model

---
+++ Models/Question.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Question
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public DateTime Date()
+        {
+            return DateTime.Now;
+        }
+
+        [Required]
+        public ApplicationUser Author { get; set; }
+
+
+
+
+
+
+    }
+}
